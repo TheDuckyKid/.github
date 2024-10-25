25e3357e487cae2e7754c94c6abb6bc48f6343f9
TheDuckyKid <26reynojaya@washk12.org>
2024-10-25 08:39:06 -0600

    listing requirements for game in py file

---
+++ timeline project/timeline.py
@@ -0,0 +1,13 @@
+print("hello timeline")
+
+#play timeline
+def timeline():
+    #1. game menu
+    #1.1 select number of players
+    #1.2 bots [on or of]
+    #1.3 difficulty [level]
+    #2. setup new game
+    #2.1 deal cards to each player
+    #2.3 pick starting player
+    #3. play game (player turns)
+    #4. show who winner is
