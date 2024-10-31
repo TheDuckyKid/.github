a1bd9ebf8f8be04034c50536ad4369244ed0cb61
TheDuckyKid <26reynojaya@washk12.org>
2024-10-31 09:17:59 -0600

    attempted to add comments to functions to explain what does what and added useless paper1.py file

--- timeline project/timeline.py
+++ timeline project/timeline.py
@@ -16,36 +16,55 @@ print("hello timeline")
     #3.4 check if player won/game is over, if true go to 4
     #3.5 go back to 3.2
     #4 show who winner is     
+ducks = {
+    ""
+}
 
 def timeline():
     print("starting timeline game")
     showGameMenu()
-    setupNewGame()
+    setupNewGame(bots,playernums)
     play()
     showWinner()
 
 def showGameMenu():
     print("test gamemenu")
+    #1 game menu
     print("Welcome to The Timeline Game")
+    #1.1 select/defining the number of players
     playernums = int(input("Please enter the amount of players you have: "))
+    #1.2 asking the user if they would like to enable bots
     bots = input("Would you like bots: (Yes/No) ")
-    return bots , playernums
+    #1.3 difficulty
+    if bots == "Yes" or bots == "yes" or bots == "Y" or bots == "y":
+        difficulty  = input("What difficulty: ")
+        return difficulty
+    return playernums
+    return bots
 
 
 def setupNewGame(playernums,bots):
+    #2 setup new game
     print("test setupNewGame")
+    #2.1 deal cards to player(s)/bots
+
+    #2.2
 
 def play():
     print("test play")
-    currentPlayer = 1
+    currentPlayer = 1 #default number for testing
+
     while not gameOver():
         takeTurn(currentPlayer)
         currentPlayer = pickNextPlayer
 
 def takeTurn():
+    #3.2 select next player 
     print(f"player{player} is taking their turn...")
 
+
 def gameOver():
+    #3.4 
     print("test gameover")
 
 def pickNextPlayer():
